259bb2efee4912fc1db1ae0f17cb3fe433a66b8d
Ahmedglez <ahmediglez@gmail.com>
2022-11-03 15:55:29 -0400

    simplificando logica de capas

--- src/main/java/cu/edu/cujae/pwebjsf/services/DriverServices.java
+++ src/main/java/cu/edu/cujae/pwebjsf/services/DriverServices.java
@@ -1,7 +1,8 @@
 package cu.edu.cujae.pwebjsf.services;
 
-import cu.edu.cujae.pwebjsf.data.DriverRepository;
 import cu.edu.cujae.pwebjsf.services.dto.DriverDto;
+import cu.edu.cujae.pwebjsf.services.repository.DriverRepository;
+
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,9 @@ public class DriverServices {
 
   public List<DriverDto> getAll() {
     return driverRepository.getAll();
+  }
+
+  public List<DriverDto> getByCategory(int codeCategory) {
+    return driverRepository.getByCategory(codeCategory).get();
   }
 }
