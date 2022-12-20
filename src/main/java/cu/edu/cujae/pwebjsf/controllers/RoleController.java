3573cb6fa68395f242c9bef8ba1887e624b86cb4
Ernesto <eaad20002504@gmail.com>
2022-12-19 23:31:17 -0500

    role controller fix

--- src/main/java/cu/edu/cujae/pwebjsf/controllers/RoleController.java
+++ src/main/java/cu/edu/cujae/pwebjsf/controllers/RoleController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1roles")
+@RequestMapping("/api/v1/roles")
 public class RoleController {
 
   @Autowired
